230aaad76a2ef98fc4f776f50c8257dd27f43bbe
Ahmed Ali <ahmedalielian20@gmail.com>
2024-08-07 00:39:58 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { SettingComponent } from './components/setting/setting.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home' ,component: HomeComponent },
+    { path: 'product', component: ProductsComponent },
+    { path: 'product/:id', component: ProductDetailsComponent },
+    {
+      path: 'setting'
+      ,component: SettingComponent,
+   
+    },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '**', component: NotFoundComponent },];
