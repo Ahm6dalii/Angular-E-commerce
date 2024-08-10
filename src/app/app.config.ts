e5b1e8268e80c0bff233a6d764dcca4c16321e92
omer-khaled98 <93747879+omer-khaled98@users.noreply.github.com>
2024-08-10 19:22:43 +0300

    Omar (#1)
    * add service Folder
    
    * provideHttpClient in app config
    
    * auth for project
    
    ---------
    
    Co-authored-by: Ahmed Ali <ahmedalielian20@gmail.com>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { AuthService } from './service/auth.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+  ],
 };
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css'], // تم التعديل هنا
 })
 export class RegisterComponent {
+  router = inject(Router);
+  errorMessege = '';
+  islodaing: boolean = false;
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(40),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[A-Za-z0-9]{6,8}'),
+    ]),
+    rePassword: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[A-Za-z0-9]{6,8}'),
+    ]),
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.pattern('01[0125][0-9]{8}'),
+    ]),
+  });
 
+  constructor(private _authService: AuthService) {}
+
+  register() {
+    if (this.registerForm.valid == false) {
+      this.registerForm.markAllAsTouched();
+    } else {
+      this.errorMessege = '';
+      this.islodaing = true;
+      console.log('Form Data:', this.registerForm.value); // طباعة بيانات النموذج
+      this._authService.register(this.registerForm.value).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.registerForm.reset();
+          setTimeout(() => {
+            this.router.navigate(['/login']);
+          }, 1000);
+        },
+        error: (err) => {
+          console.error('Error Response:', err);
+          this.islodaing = false;
+          this.errorMessege = err.error.message;
+        },
+      });
+    }
+  }
 }
